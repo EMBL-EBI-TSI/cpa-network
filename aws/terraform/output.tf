f55023c3c800cecc32afab00fe1deb6b81ce38e8
Gianni Dalla Torre <gianni@ebi.ac.uk>
2017-09-25 15:03:10 +0100

    Add output parameters in AWS

--- aws/terraform/output.tf
+++ aws/terraform/output.tf
@@ -1,3 +1,11 @@
 output "network_id" {
+  value = "${aws_vpc.vpc.id}"
+}
+
+output "network_name" {
+  value = "${var.network_name}"
+}
+
+output "subnet_id" {
   value = "${aws_subnet.subnet.id}"
 }
