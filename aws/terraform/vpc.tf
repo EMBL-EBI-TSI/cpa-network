99e1f8f34baaf6d366a14abdab225b4ddfb1b4c3
Gianni Dalla Torre <gianni@ebi.ac.uk>
2017-09-26 13:50:58 +0100

    Add default route for the AWS-VPC

--- aws/terraform/vpc.tf
+++ aws/terraform/vpc.tf
@@ -23,3 +23,9 @@ resource "aws_internet_gateway" "gateway" {
     Name = "${var.network_name}-gateway"
   }
 }
+
+resource "aws_route" "route" {
+  route_table_id         = "${aws_vpc.vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.gateway.id}"
+}
