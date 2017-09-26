dc76e3963ef8f0059fcc0787a865ce6b8d3ee6fe
erikvdbergh <erikvdbergh@gmail.com>
2017-09-26 16:59:51 +0100

    edit to use env variables for auth

--- azure/terraform/variables.tf
+++ azure/terraform/variables.tf
@@ -10,18 +10,18 @@ variable "region" {
 
 # For help on getting the 4 variables below, see: https://www.terraform.io/docs/providers/azurerm/aut
 
-variable "subscription_id" {
-  description = "ID of the Azure subscription to use"
-}
-
-variable "client_id" {
-  description = "ID of the Azure client to use"
-}
-
-variable "client_secret" {
-  description = "Value of the Azure client secret"
-}
-
-variable "tenant_id" {
-  description = "ID of the Azure tenant to use"
-}
+#variable "subscription_id" {
+#  description = "ID of the Azure subscription to use"
+#}
+#
+#variable "client_id" {
+#  description = "ID of the Azure client to use"
+#}
+#
+#variable "client_secret" {
+#  description = "Value of the Azure client secret"
+#}
+#
+#variable "tenant_id" {
+#  description = "ID of the Azure tenant to use"
+#}
